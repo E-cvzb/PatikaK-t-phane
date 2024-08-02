c60cd8e1635d34f8c01452719c0e95264c992163
Emrullah Cevizbaş <cevizbas01@gmail.com>
2024-08-02 16:52:16 +0300

    Proje dosyası ekle.

---
+++ PatikaKütüphane/KütüphaneVeri.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaKütüphane
+{
+    
+    public class KütüphaneVeri
+    {
+        public static int _kitapSayısı;// nesne örneği kullanmadan sayaç oluşturmak için static şeklinde oluşturulmuştur
+
+         static KütüphaneVeri()//static formunda sayaç oluşturma 
+        {
+            _kitapSayısı = 0;
+        }
+        public static int kitapSayısı
+        {
+            get { return _kitapSayısı; }
+            set { _kitapSayısı= value; }
+
+        }
+
+        public static void KitapSayısı()// sayaç yazdırma
+        {
+            Console.WriteLine($"Kayıt edilen toplam kitap sayısı--------> {_kitapSayısı}");
+        }
+        public string _kitapAd;
+        public string _yazarİsim;
+        public string _yazarSoyisim;
+        public int _sayfaSayısı;
+        public string _yayınEvi;
+        public DateTime _yayınTarihi;
+        
+
+        public string kitapAd
+        {
+            get { return _kitapAd; }
+            set { _kitapAd = value; }
+        }
+        public string yazarİsimi
+        {
+            get
+            {
+                return _yazarİsim;
+            }
+            set
+            {
+                _yazarİsim = value;
+            }
+        }
+        public string yazarSoyisim
+        {
+            get { return _yazarSoyisim; }
+            set { _yazarSoyisim = value;}
+
+        }
+        public int sayfaSayısı
+        {
+            get { return _sayfaSayısı; }
+            set
+            {
+                if (value > 0)//kitap sayısının 0 dan büyük olması şartı
+                {
+                    _sayfaSayısı = value;
+                }
+            }
+
+        }
+        public string yayınEvi
+        {
+            get { return _yayınEvi;}
+            set { _yayınEvi = value;}
+
+        }
+        public DateTime yayınTarihi
+        {
+            get { return _yayınTarihi; }
+            set { _yayınTarihi = value;}
+        }
+        public KütüphaneVeri(string kitapad ,string yazarİsim , string yazarSoyisim, int sayfasayısı,string yayınEvi) 
+        {
+
+            _kitapAd = kitapAd;
+            _yazarİsim = yazarİsim;
+            _yazarSoyisim= yazarSoyisim;
+            _sayfaSayısı = sayfasayısı;
+            _yayınEvi = yayınEvi;
+            _yayınTarihi= DateTime.Now;// kayıt yapılan starihin şu an olmasını sağlıyor
+            _kitapSayısı++;//Metot her kullanıldığında sayaç da artacak 
+
+            
+        }
+        public void BilgiYazma()//girilen bilgiler ile siteme kayıt edilme şekili 
+        {
+            Console.WriteLine($"{yazarİsimi}{yazarSoyisim}'nin {sayfaSayısı} sayfalık {yayınEvi}yayın evinden {kitapAd} kitabı kütüphaneye {yayınTarihi} tarihinde kayıt edilmiştir");
+
+        }
+        public KütüphaneVeri()//defaut yapı
+        {
+        }
+    }
+
+   
+    
+}
---
+++ PatikaKütüphane/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PatikaKütüphane;
+  
+
+Console.WriteLine("Merhabalar Patika Kütüphanesine hosşgeldiniz ");
+KütüphaneVeri kitap=new KütüphaneVeri();
+kitap.yazarİsimi = "Ahmet";
+kitap.yazarSoyisim = "Yılmaz";
+kitap.yayınTarihi= DateTime.Now;
+kitap.sayfaSayısı = 320;
+kitap.yayınEvi = "Alfa Yayınları";
+kitap.kitapAd = "Bir Kitap";
+kitap.BilgiYazma();
+KütüphaneVeri._kitapSayısı++;// static türünde tanımlanan  değişkenler verilen sınıf adı ile  birlikte yazılır 
+
+
+
+KütüphaneVeri kitap1 = new KütüphaneVeri("Başak Bir Kitap", "Ayşe","Karan",215,"Beta Yayıncılık");
+kitap1.BilgiYazma();
+
+KütüphaneVeri kitap2 = new KütüphaneVeri("Daha Başka Bir Kitap","Mehmet","Demir",450,"Gama Yayıncılık");
+kitap2 .BilgiYazma();
+
+KütüphaneVeri kitap3 = new KütüphaneVeri("Son Kitap", "Elif", "Şahin", 275, "Deta Yayıncılık");
+kitap3 .BilgiYazma();
+
+KütüphaneVeri kitap4 = new KütüphaneVeri("Örnek Kitap","Selin","Yurt",198,"Epsilon Yyınları");
+kitap4 .BilgiYazma();
+
+    KütüphaneVeri.KitapSayısı();// toplmda kaç tane kitap kayıt edildiğini yazdıracak
+
+
+
+
+
+
+
